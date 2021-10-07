e9aae1340665c7ed961543ae1a8016d15f046802
radRoy <daniel.walther@uzh.ch>
2021-10-07 23:05:27 +0200

    fixed apparent plot-weird; made some comments for overview (survival)

--- course-results/2021/sarcoma-group/kaplan_meier_plotting.py
+++ course-results/2021/sarcoma-group/kaplan_meier_plotting.py
@@ -1,9 +1,11 @@
 from lifelines import KaplanMeierFitter
 from lifelines.datasets import load_waltons
+import matplotlib.pyplot as plt
+
 waltons = load_waltons()
 
 kmf = KaplanMeierFitter(label="waltons_data")
 kmf.fit(waltons['T'], waltons['E'])
 kmf.plot()
 
-print(type(kmf))
+plt.show()
--- course-results/2021/sarcoma-group/survival_analysis_sarcoma.py
+++ course-results/2021/sarcoma-group/survival_analysis_sarcoma.py
@@ -19,51 +19,15 @@ from lifelines import KaplanMeierFitter
 
 
 #%% Data Handling
-"""
-our goals / todos:
 
-- import gene_datasets (4)
-- import survival_dataset (1)
-=> do not combine them into 1 file (not recommended by Rahel)
-
-- filter the datasets for interesting variables (columns), do this with a copy of the datasets
-
-- make boxplots of ...
-- make kaplan-meier of ...
-"""
-
-def extract_id_from_csv(filename):
-    """
-    attempt at extractin ids from some csv files (should the same process for Survival and Gene .csv files).
-    :param filename: str of the filename to be scanned for ids
-    :return: list ids containing all the ids (of the lines) of the input file.
-    """
-    ids = []
-
-    with open('sarcoma erbb2.csv') as erbb2:
-        for i, line in enumerate(erbb2):
-            ids = ids.append(list(line))
-
-"""id0 = '5bab56c0727983b2e00aaa1b'
-id1 = '5bab56c0727983b2e00aaab7'"""
-
-
-#%% EDA (Exploratory Data Analysis)
-
-
-#%% Kaplan-Meier plot (survival plot)
-
-
-#%% Barplot of CNV fraction (gene comparison)
-
-
-#%% Communication
-
-#%% - - -
-#%% Functions to get gene data (cheatsheet)
-
+# Functions to get gene data (cheatsheet)
 
 def transform_to_df(data):
+    """
+    transform ... data format to pandas data frame format.
+    :param data: input dataset of type
+    :return: same dataset but as a pandas data frame (pd.DataFrame)
+    """
     data = pd.DataFrame(data)
     data.columns = data.iloc[0]
     data = data[1:]
@@ -71,14 +35,15 @@ def transform_to_df(data):
 
 
 def get_gene_data(cancer_type, gene_name):
+
     with open(cancer_type + '.csv', 'r') as read_obj:
         csv_reader = csv.reader(read_obj)
         cancer = list(csv_reader)
-        # header = cancer[0]
+        header = cancer[0]  # the header row of the data set
 
     ids = []
     for id in cancer:
-        ids.append(id[14])
+        ids.append(id[14])  # extract the ids of ... what measurement?
 
     with open(cancer_type + '_' + gene_name + '.csv', 'r') as read_obj:
         csv_reader = csv.reader(read_obj)
@@ -87,9 +52,6 @@ def get_gene_data(cancer_type, gene_name):
 
     gene_data = []
     for line in gene_all:
-        """print(line[14])
-        print(line[16])
-        print()"""
         if line[16] in ids: # previously line[14]
             gene_data.append(line)
 
@@ -98,16 +60,16 @@ def get_gene_data(cancer_type, gene_name):
 
     print(len(ids), len(gene_data))
 
-    return cancerdf, genedf
+    return cancerdf, genedf, cancer  # added return variable 'cancer', a list version of the cancer gene CNV data set
 
-# calling the functions to do the actual work
-sarcoma, tp53 =  get_gene_data('sarcoma', 'tp53del')
+# calling the functions for saving data sets to usable variables here.
+sarcoma, tp53, tp53_list =  get_gene_data('sarcoma', 'tp53del')
+    # survival set (pd.dataframe), cancer gene CNV set (pd.dataframe),
+
 sarcomanum = sarcoma.apply(pd.to_numeric, errors='coerce').fillna(sarcoma)
+    # changes format from pd.dataframe to some numeric format (which one? what changed, simply no column names?)
 
-print(sarcomanum['info.followupMonths'])
-
-sarcomanum_km
-
+#print(sarcomanum['info.followupMonths'])
 
 """ gene names of the other genes
 get_gene_data('sarcoma erbb2.csv')
@@ -117,7 +79,16 @@ get_gene_data('sarcoma cdkn2a.csv')
 """
 
 
-#%% KM analysis (Kaplan-Meier / Survival Plot) (cheatsheet)
+#%% EDA (Exploratory Data Analysis)
+
+
+#%% Kaplan-Meier plot (survival plot)
+"""plots: weird behaviour of plotting windows:
+if either one kind of plot is drawn, the other one can not be drawn within the same execution.
+idea: are both plotted when the plot output is saved to a file? (then only pycharm plotting is buggy)
+idea: is it an issue with the multi plot usage (plt.subplot())?
+"""
+
 
 NCIT_surv = sarcomanum['histologicalDiagnosis.label'].unique()
 
@@ -137,6 +108,14 @@ while i < len(NCIT_surv):
 plt.show()
 
 
+#%% Barplot of CNV fraction (gene comparison)
+
+
+#%% Communication
+
+
+#%% KM analysis (Kaplan-Meier / Survival Plot) (cheatsheet)
+
 #%% Boxplot (CNV fraction) (cheatsheet)
 
 
@@ -149,6 +128,8 @@ while i < len(NCIT_box):
     ax.set_xticklabels(['cnvcoverage','delcoverage','dupcoverage','death'],rotation=180) # rotation=90 changed to 180
     ax.set_title(NCIT_box[i],fontsize=7)
     i = i + 1
+
+plt.show()
 
 """boxplot(fontsize=5)
 ax.set_xticklabels(['cnvcoverage','delcoverage','dupcoverage','death'],rotation=180) # rotation=90 changed to 180
