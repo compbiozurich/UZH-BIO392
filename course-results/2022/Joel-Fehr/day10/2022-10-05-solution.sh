3a90c866d15bbb90651df331b24bc483ff600a6c
Joel Fehr <63153161+jf-11@users.noreply.github.com>
2022-10-05 10:51:42 +0200

    Update 2022-10-05-solution.sh

--- course-results/2022/Joel-Fehr/day10/2022-10-05-solution.sh
+++ course-results/2022/Joel-Fehr/day10/2022-10-05-solution.sh
@@ -1,5 +1,11 @@
-samtools view -b -f PROPER_PAIR ../data/alignments/wildtype/APC_wt.sam | samtools sort > ../data/alignments/wildtype/APC_wt_proper.bam
+#!/bin/env/bash
+set -euo pipefail
+
+# Using samtools view, we convert the alignments generated in the previous step to (binary) BAM format. 
+# Then, with samtools sort, we sort the alignments by their starting coordinates.
+# Finally, we generate indices for our alignments with samtools index
+samtools view -b -f 0x2 ../data/alignments/wildtype/APC_wt.sam | samtools sort > ../data/alignments/wildtype/APC_wt_proper.bam
 samtools index ../data/alignments/wildtype/APC_wt_proper.bam
 
-samtools view -b -f PROPER_PAIR ../data/alignments/mutated//APC_mut.sam | samtools sort > ../data/alignments/mutated/APC_mut_proper.bam
+samtools view -b -f 0x2 ../data/alignments/mutated//APC_mut.sam | samtools sort > ../data/alignments/mutated/APC_mut_proper.bam
 samtools index ../data/alignments/mutated/APC_mut_proper.bam
