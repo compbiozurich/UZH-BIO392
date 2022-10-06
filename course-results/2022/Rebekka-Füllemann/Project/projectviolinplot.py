9f13d14360610390355c9319ec19a60af18515cf
JanneBerger <113997444+JanneBerger@users.noreply.github.com>
2022-10-06 12:44:04 +0200

    Create projectviolinplot.py

---
+++ course-results/2022/Rebekka-Füllemann/Project/projectviolinplot.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import kaplanmeier as km
+import matplotlib.pyplot as plt
+from lifelines import KaplanMeierFitter
+import seaborn as sns
+
+# violinplot
+ax[1, 0].violinplot([dataset["TP53_biosample.tsv"], dataset["MYC_biosample.tsv"]])
+ax[1, 0].set_xticks([1, 2])
+ax[1, 0].set_xticklabels(["TP53", "MYC"])
+ax[1, 0].set(xlabel = "Genes", ylabel = "Expression")
+ax[1, 0].set_title("violinplot_1")
