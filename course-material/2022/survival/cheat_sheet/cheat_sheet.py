90037a6f811833cdaf8280abc47263e2a2bc15a0
Rahel Paloots <rahelpaloots@gmail.com>
2022-10-06 09:22:20 +0200

    Update cheat_sheet.py

--- course-material/2022/survival/cheat_sheet/cheat_sheet.py
+++ course-material/2022/survival/cheat_sheet/cheat_sheet.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import kaplanmeier as km
 import matplotlib.pyplot as plt
-
+from lifelines import KaplanMeierFitter
 
 ######### matching ids ####################
 # read the sample datasets
